5021fdf52625c61b40a0802fee1bd6aee7aa50ce
rogermylife Hu <rogermylifeQ@gmail.com>
2016-12-28 15:28:57 +0800

    Ver 8.1.0

--- lab10_byteorder_socket/socket/client.c
+++ lab10_byteorder_socket/socket/client.c
@@ -4,12 +4,18 @@
 #include<netinet/in.h>
 #include<string.h>
 
-int main()
+int main(int argc,char *argv[])
 {
+    if(argc<3)
+    {
+        printf("./client <server_ip_address> <port>\n");
+        exit(0);
+    }
     int clientFD = socket(AF_INET,SOCK_STREAM,0);
-    struct hostent *server = gethostbyname("localhost");
+    struct hostent *server = gethostbyname(argv[1]);
     struct sockaddr_in cout;
     char buf[256];
+    
     if(server==NULL)
     {
         printf("no such host\n");
@@ -18,18 +24,23 @@ int main()
     bzero(&cout,sizeof(cout));
     cout.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&cout.sin_addr.s_addr, server->h_length);
-    cout.sin_port = htons(9999);
+    cout.sin_port = htons(atoi(argv[2]));
 
     if(connect(clientFD,&cout,sizeof(cout))<0)
     {
         printf("connect error\n");
         exit(1);
     }
-    bzero(buf,256);
-    int n=read(clientFD,buf,255);
-    if(n<0)
+    while(1)
     {
-        printf("read error");
+        bzero(buf,256);
+        int n=read(clientFD,buf,255);
+        if(n<0)
+        {
+            printf("read error");
+        }
+        if(n==0)
+            exit(0);
+        printf ("read %d %s",n,buf);
     }
-    print ("%s\n",buf);
 }
--- lab10_byteorder_socket/socket/server.c
+++ lab10_byteorder_socket/socket/server.c
@@ -6,24 +6,62 @@
 #include<netdb.h>
 #include<unistd.h>
 #include<strings.h>
-
+#include<sys/sysinfo.h>
+#include<signal.h>
+void sig_fork(int signo)
+{
+        pid_t pid;
+        int stat;
+        pid=waitpid(0,&stat,WNOHANG);
+        printf("child died\n");
+}
 void process(int clientFD)
 {
     int n;
     char buffer[256];
     bzero(buffer,256);
-    n=write(clientFD,"GOGOGO\n",7);
+    //n=write(clientFD,"GOGOGO\n",7);
+    struct sysinfo si;
+    while(1)
+    {
+        sysinfo(&si);
+        sprintf(buffer,"ps count %d\n",si.procs);
+        n=write(clientFD,buffer,strlen(buffer));
+        sleep(1);
+    }
 }
-int main()
+int main(int argc ,char *argv[])
 {
+    signal (SIGCHLD, sig_fork);
     struct sockaddr_in sin;
     int pid;
     int serverFD = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    int reuse=1;
+    if(argc<2)
+    {
+        printf("./server <port>\n");
+        exit(0);
+    }
+    if(serverFD<0)
+    {
+        printf("socket error\n");
+        exit(1);
+    }
+    if (setsockopt(serverFD, SOL_SOCKET, SO_REUSEADDR, &reuse,sizeof(int)) < 0)
+    {
+        printf("set socket error");
+        exit(1);
+    }
     bzero(&sin,sizeof(sin));
     sin.sin_family=AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port=htons(9999);
-    bind(serverFD,&sin,sizeof(sin));
+    sin.sin_port=htons(atoi(argv[1]));
+    if(bind(serverFD,&sin,sizeof(sin))<0)
+    {
+        printf("bind error\n");
+        exit(1);
+    }
+    
     listen(serverFD,20);
     while(1)
     {
@@ -32,6 +70,7 @@ int main()
         int clientFD = accept(serverFD,&sout,&clen);
         if(clientFD < 0)
             printf("error on accept\n");
+        printf ("accept a child\n");
         pid=fork();
         if(pid==0)
         {
